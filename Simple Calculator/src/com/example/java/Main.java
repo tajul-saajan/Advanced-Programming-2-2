6b9f8fb6a4d88a91fcc7c8e806129df7cdf83fd9
tajul.saajan <tajul.saajan@gmail.com>
2017-10-11 00:06:50 +0600

    added user input

--- Simple Calculator/src/com/example/java/Main.java
+++ Simple Calculator/src/com/example/java/Main.java
@@ -1,14 +1,25 @@
 package com.example.java;
 
+import java.util.Scanner;
+
 public class Main{
 	
 	public static void main(String args[]) {
-		int a = 20, b = 10;
+		int a, b,c;
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
+		a=scanner.nextInt();
+		System.out.print("Enter the second number : ");
+		b=scanner.nextInt();
+		System.out.print("Select an operation : 1.Addition\n2.Subtraction\n3.Multiplication\n4.Division : ");
+		c=scanner.nextInt();
 		My_Calculation demo = new My_Calculation();
-		demo.addition(a, b);
-		demo.Substraction(a, b);
-		demo.multiplication(a, b);
-		demo.division(a, b);
+		if(c==1)demo.addition(a, b);
+		else if(c==2)demo.Substraction(a, b);
+		else if(c==3)demo.multiplication(a, b);
+		else if(c==4)demo.division(a, b);
+		else System.out.println("\nInvalid selection\n");
 	}
 	
 }
--- Simple Calculator/src/com/example/java/My_Calculation.java
+++ Simple Calculator/src/com/example/java/My_Calculation.java
@@ -5,24 +5,24 @@ class Calculation {
 
 	public void addition(int x, int y) {
 		z = x + y;
-		System.out.println("The sum of the given numbers:" + z);
+		System.out.println("\nThe sum of the given numbers:" + z);
 	}
 
 	public void Substraction(int x, int y) {
 		z = x - y;
-		System.out.println("The difference between the given numbers:" + z);
+		System.out.println("\nThe difference between the given numbers:" + z);
 	}
 }
 
 public class My_Calculation extends Calculation {
 	public void multiplication(int x, int y) {
 		z = x * y;
-		System.out.println("The product of the given numbers:" + z);
+		System.out.println("\nThe product of the given numbers:" + z);
 	}
 
 	public void division(int x,int y) {
 		z=x/y;
-		System.out.println("The value of "+x+"/"+y+" is "+z);
+		System.out.println("\nThe value of "+x+"/"+y+" is "+z);
 		
 	}
 }
