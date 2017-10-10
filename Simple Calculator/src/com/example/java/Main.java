509b456cedaaab530851513a6211d6c44a2b1698
tajul.saajan <tajul.saajan@gmail.com>
2017-10-10 23:49:45 +0600

    adding the Simple Calculator project to the git repo

---
+++ Simple Calculator/src/com/example/java/Main.java
@@ -0,0 +1,14 @@
+package com.example.java;
+
+public class Main{
+	
+	public static void main(String args[]) {
+		int a = 20, b = 10;
+		My_Calculation demo = new My_Calculation();
+		demo.addition(a, b);
+		demo.Substraction(a, b);
+		demo.multiplication(a, b);
+		demo.division(a, b);
+	}
+	
+}
