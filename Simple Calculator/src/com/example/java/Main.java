b93688662cc5b186b697b2d683706cb35f534261
tajul.saajan <tajul.saajan@gmail.com>
2017-10-11 15:03:39 +0600

    some functions added

--- Simple Calculator/src/com/example/java/Main.java
+++ Simple Calculator/src/com/example/java/Main.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Main{
 	
 	public static void main(String args[]) {
-		int a, b,c,z;
+		int a = 0,b=0,c=0,z=0;
 		System.out.println("Select an action : 1.Alzebra\n2.Geometry\n\n");
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		z=scanner.nextInt();
 		if (z==1) {
-			System.out.print("Enter the first number : ");
-			a=scanner.nextInt();
-			System.out.print("Enter the second number : ");
-			b=scanner.nextInt();
-			System.out.print("Select an operation : 1.Addition\n2.Subtraction\n3.Multiplication\n4.Division : ");
+
+			System.out.print("Select an operation : 1.Addition\n2.Subtraction\n" +
+					"3.Multiplication\n4.Division\n5.Exponent \n6.Square Root\n");
 			c=scanner.nextInt();
 			My_Calculation demo = new My_Calculation();
 			if(c==1)demo.addition(a, b);
             else if(c==2)demo.Substraction(a, b);
             else if(c==3)demo.multiplication(a, b);
             else if(c==4)demo.division(a, b);
+			else if(c==5)demo.exponent(a,b);
+			else if (c==6)demo.squareRoot(a);
             else System.out.println("\nInvalid selection\n");
 		}
 		else if(z==2){
--- Simple Calculator/src/com/example/java/My_Calculation.java
+++ Simple Calculator/src/com/example/java/My_Calculation.java
@@ -1,14 +1,25 @@
 package com.example.java;
 
+import java.util.Scanner;
+
 class Calculation {
 	int z;
+	Scanner scanner=new Scanner(System.in);
 
 	public void addition(int x, int y) {
+		System.out.print("Enter the first number : ");
+		x=scanner.nextInt();
+		System.out.print("Enter the second number : ");
+		y=scanner.nextInt();
 		z = x + y;
 		System.out.println("\nThe sum of the given numbers:" + z);
 	}
 
 	public void Substraction(int x, int y) {
+		System.out.print("Enter the first number : ");
+		x=scanner.nextInt();
+		System.out.print("Enter the second number : ");
+		y=scanner.nextInt();
 		z = x - y;
 		System.out.println("\nThe difference between the given numbers:" + z);
 	}
@@ -16,13 +27,32 @@ class Calculation {
 
 public class My_Calculation extends Calculation {
 	public void multiplication(int x, int y) {
+		System.out.print("Enter the first number : ");
+		x=scanner.nextInt();
+		System.out.print("Enter the second number : ");
+		y=scanner.nextInt();
 		z = x * y;
 		System.out.println("\nThe product of the given numbers:" + z);
 	}
 
 	public void division(int x,int y) {
+		System.out.print("Enter the first number : ");
+		x=scanner.nextInt();
+		System.out.print("Enter the second number : ");
+		y=scanner.nextInt();
 		z=x/y;
 		System.out.println("\nThe value of "+x+"/"+y+" is "+z);
 		
+	}
+	public void exponent(int x,int y){
+
+		z=(int) Math.pow(x,y);
+		System.out.println("\nThe value of "+x+"/"+y+" is "+z);
+	}
+	public void squareRoot(int x){
+		System.out.print("Enter the number : ");
+		x=scanner.nextInt();
+		double kk = Math.sqrt(x);
+		System.out.println("\nThe value of Square Root of "+x+" is "+kk);
 	}
 }
